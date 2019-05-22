c70378fc85aa1e062962efd307733766acd21290
Mehreen Butt <mbutt@republiccharterschools.org>
2019-05-22 14:13:18 -0500

    updated remove button

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 var cardContainer = $('.card-container');
-
-$('.summer-opp-btn').on("click", appendNewOpportunity);
+var summerbutton = $('.summer-opp-btn')
+summerbutton.on("click", appendNewOpportunity);
 
 function appendNewOpportunity() {
   cardContainer.append(`
     <div>
       <p class="new-opp-card">AI4ALL
         <i class="fa fa-heart" style="font-size:20px;color:black;"></i>
-        <button>Remove</button>
+        <button class="delete">Remove</button>
       </p>
     </div>
-    `)
+    `);
+
+    var completed = $('.delete');
+    completed.on("click", remove);
+}
+
+function remove () {
+  event.target.parentNode.remove()
 }
